b7b3238d01577fddbe5b186436563126edb981a0
James Bacon <james@baconi.co.uk>
2023-02-04 14:21:42 +0000

    Basic Discord4J ping/pong integration.

--- libraries/discord4j/build.gradle.kts
+++ libraries/discord4j/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 
 dependencies {
     implementation(project(":common"))
+    implementation("com.discord4j:discord4j-core:3.2.3")
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.14.2")) // security patching discord4j-core
+    implementation(platform("io.projectreactor:reactor-bom:2020.0.27")) // security patching discord4j-core
 }
 
 application {
--- libraries/discord4j/src/main/kotlin/uk/co/baconi/playground/discord/discord4j/Main.kt
+++ libraries/discord4j/src/main/kotlin/uk/co/baconi/playground/discord/discord4j/Main.kt
@@ -1,5 +1,65 @@
 package uk.co.baconi.playground.discord.discord4j
 
-fun main(args: Array<String>) {
-    println("Hello, World! discord4j")
+import discord4j.core.DiscordClient
+import discord4j.core.GatewayDiscordClient
+import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.event.domain.message.MessageCreateEvent
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Mono.empty
+import reactor.core.publisher.Mono.fromRunnable
+import uk.co.baconi.playground.discord.common.BotConfiguration
+
+fun main() {
+
+    val config = BotConfiguration()
+
+    val logger = LoggerFactory.getLogger("uk.co.baconi.playground.discord.discord4j.Main").apply {
+        info("Discord4J server is starting")
+    }
+
+    val client = DiscordClient.create(config.token)
+
+    val login = client.withGateway { gateway: GatewayDiscordClient ->
+
+        gateway.on(ReadyEvent::class.java) { event ->
+            fromRunnable<Void> {
+                logger.info("Discord4J server is ready as {}#{}", event.self.username, event.self.discriminator)
+            }
+        }.subscribe()
+
+        gateway.on(MessageCreateEvent::class.java) { event ->
+            when {
+                event.message.author.isEmpty -> empty()
+                event.message.author.get().isBot -> empty()
+                event.message.content.equals("!ping", true) -> {
+                    logger.debug("Received ping via shortcut")
+                    event.message.channel.flatMap { channel ->
+                        channel.createMessage("pong!")
+                    }
+                }
+                event.message.content.equals("${config.applicationMention} ping", true) -> {
+                    logger.debug("Received ping via mention")
+                    event.message.channel.flatMap { channel ->
+                        channel.createMessage("pong!")
+                    }
+                }
+                else -> empty()
+            }
+        }.subscribe()
+
+        gateway.on(ApplicationCommandInteractionEvent::class.java) { event ->
+            when {
+                event.commandName != "ping" -> empty()
+                else -> {
+                    logger.debug("Received ping via slash command")
+                    event.reply("pong!")
+                }
+            }
+        }.subscribe()
+
+        gateway.onDisconnect()
+    }
+
+    login.block()
 }
